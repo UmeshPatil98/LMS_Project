3c62f93fa9d6e5725445deb884d51427d8c80c3b
umeshpatil98 <srinupatil98@gmail.com>
2023-08-31 18:00:24 +0530

    commit changes

--- models/user.model.js
+++ models/user.model.js
@@ -33,7 +33,8 @@ const userschema = new schema({
     role:{
         type:String,
         enum:['USER','ADMIN'],
-        default:'USER'
+        default:'USER',
+        default:'ADMIN'
     },
 
     avatar:{
